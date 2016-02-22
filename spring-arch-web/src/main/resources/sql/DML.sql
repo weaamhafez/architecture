7f539729b8dde67a4bb670b120eb73d2b552b8fe
weaamhafez <weaam.hafez@gmail.com>
2016-02-22 14:18:15 +0400

    DML update

--- spring-arch-web/src/main/resources/sql/DML.sql
+++ spring-arch-web/src/main/resources/sql/DML.sql
@@ -1,2 +1,2 @@
-INSERT INTO `user` VALUES (1, 'freelancer', 'FreeLancer', 'freelancer@freelancer.com', '05666666', '$2a$10$gp4MpER16y47Dk..lDlpM.SrRKXLwwu/mBzOcoid.0l/ApUQpCPQC', '', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES (2, 'test', 'Test User', 'freelancer@freelancer.com', '05633669', '$2a$10$gp4MpER16y47Dk..lDlpM.SrRKXLwwu/mBzOcoid.0l/ApUQpCPQC', '', NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES (1, 'freelancer', 'FreeLancer', 'freelancer@freelancer.com', '05666666', '$2a$10$gp4MpER16y47Dk..lDlpM.SrRKXLwwu/mBzOcoid.0l/ApUQpCPQC', '1', NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES (2, 'test', 'Test User', 'freelancer@freelancer.com', '05633669', '$2a$10$gp4MpER16y47Dk..lDlpM.SrRKXLwwu/mBzOcoid.0l/ApUQpCPQC', '1', NULL, NULL, NULL, NULL);
